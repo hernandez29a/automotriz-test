067e933daf25439e3b85d9d8d7bade2fda0d8959
Gustavo Hernandez <hernandez29a@gmail.com>
2023-06-10 17:06:13 -0400

    wip duplicity excepcion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     //MongooseModule.forRoot(process.env.MONGODB),
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-kenility'),
     UserModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
