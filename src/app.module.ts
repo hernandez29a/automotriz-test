16c79f7897b44b605e81e0dca95a3ce8d3b94d5f
Gustavo Hernandez <hernandez29a@gmail.com>
2023-06-10 13:15:13 -0400

    wip add mongoose library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    //MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-kenility'),
+    UserModule,
   ],
   controllers: [],
   providers: [],
